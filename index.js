d4454efbf04f6346286cfb8d29b63511b6c47fab
Mathieu Garcia <mgarcia@wiseflat.com>
2015-04-05 18:12:02 +0200

    evo - add version from package.json

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var xplteleinfo = require("./lib/xpl-teleinfo");
 var schema_teleinfobasic = require('/etc/wiseflat/schemas/teleinfo.basic.json');
+var schema_teleinfoconfig = require('/etc/wiseflat/schemas/teleinfo.config.json');
 
 var wt = new xplteleinfo(null, {
 	xplLog: false,
@@ -14,7 +15,16 @@ wt.init(function(error, xpl) {
 	}
 
 	xpl.addBodySchema(schema_teleinfobasic.id, schema_teleinfobasic.definitions.body);
-
+	xpl.addBodySchema(schema_teleinfoconfig.id, schema_teleinfoconfig.definitions.body);
+	
+        // Load config file into hash
+        wt.readConfig();
+        
+        // Send every minutes an xPL status message 
+        setInterval(function(){
+                wt.sendConfig();
+        }, 60 * 1000);
+	
         xpl.on("xpl:teleinfo.basic", function(evt) {
 		//console.log(evt);
                 wt.validBasicSchema(evt.body);
